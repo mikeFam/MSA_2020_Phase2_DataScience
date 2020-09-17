9041e17bd93451ce6a8e6199d71ffe11bdd5fafc
Michael Pham <tpha585@aucklanduni.ac.nz>
2020-09-18 07:32:43 +1200

    Business Case

---
+++ Business_Case/keys.py
@@ -0,0 +1,5 @@
+#These keys are examples only, they won't work.
+#This is what the keys file should contain.
+client_id='NDnx5PT_B1XMyg'
+client_secret='mB4CX0x7uDKt46DhNTKJc5ml-tg'
+username='CosmicGoose98'
